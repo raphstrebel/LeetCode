835562e0b03b628ae560f60fb1c54ea424f939a2
rsefraou <raphstrebel@gmail.com>
2022-02-17 18:34:41 +0100

    DS 1: day 15: part 3: lowest common ancestor in BST (iterative approach)

--- Data Structures 1/32. lowest-common-ancestor-of-a-binary-search-tree.py
+++ Data Structures 1/32. lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,7 +6,36 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestorIt(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+        Iterative approach:
+        - if p and q are in different subtrees, return root
+        - if p and q are in same subtree, loop over common subtree
+        """
+        if p.val == q.val:
+            return p
+        # set p < q (to simplify)
+        if p.val > q.val:
+            tmp = q
+            q = p
+            p = tmp
+        node = root
+        while node is not None:
+            if p.val <= node.val:
+                if node.val <= q.val:
+                    return node
+                else:
+                    node = node.left
+            else:
+                node = node.right
+            
+        return node
+    def lowestCommonAncestorRec(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+        Recursive approach: 
+        - if p and q are in left and right subtrees of root (resp.), return root
+        - otherwise set root = root.left (if p,q < root) or root.right (if p,q > root)
+        """
         if p.val == q.val:
             return p
         if p.val > q.val:
