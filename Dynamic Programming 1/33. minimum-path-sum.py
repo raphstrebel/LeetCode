62c732e36c9dd9fd7b4e11aeea6cac0d32e300d5
raphstrebel <raphael.strebel@epfl.ch>
2022-04-13 16:01:07 +0200

    DP 1: day 16: part 1: minimum path sum with linear additional space (without changing input)

--- Dynamic Programming 1/33. minimum-path-sum.py
+++ Dynamic Programming 1/33. minimum-path-sum.py
@@ -1,5 +1,32 @@
 class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
+    def minPathSumLinearSpace(self, grid: List[List[int]]) -> int:
+        """Assuming we may NOT change the input -> O(N) extra space"""
+        
+        n = len(grid)
+        m = len(grid[0])
+        # initialize r to be the last element
+        r = [grid[n-1][m-1]] * m
+        # iterate from down-right to top-left, keeping the min path sum at each row
+        for i in range(n-1, -1, -1):
+            # for every line, r contains the min path sum until every column
+            # so we need to keep only an array of length M
+            # r[j] contains either the sum until r[j+1] (the path sum on the right of j)
+            # or the sum until r[j] (the path sum until the element below j, which was computed on the previous row "i-1")
+            for j in range(m-1, -1, -1):
+                if i == n-1 and j == m-1:
+                    # bottom-right value was initialized
+                    continue
+                elif i == n - 1 and j != m - 1:
+                    # if last row, then consider only the value on the right (next column)  
+                    r[j] = grid[i][j] + r[j+1]
+                elif i != n - 1 and j == m - 1:
+                    # if last column, then consider only the value below (row i-1) 
+                    r[j] = grid[i][j] + r[j]
+                else:
+                    # consider the minimum between the value below and value on the right
+                    r[j] = grid[i][j] + min(r[j], r[j+1])
+        return r[0]
+    def minPathSumChangeInput(self, grid: List[List[int]]) -> int:
         """Assuming we may change the input"""
         
         n = len(grid)
