a06b02efa56e896162df834ae5a45b4c975fabf9
raphstrebel <raphael.strebel@epfl.ch>
2022-04-18 14:26:06 +0200

    DP 1: day 18: part 2: greedy wiggle subsequence O(N) time and O(1) space

--- Dynamic Programming 1/38. wiggle-subsequence.py
+++ Dynamic Programming 1/38. wiggle-subsequence.py
@@ -1,6 +1,42 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        """Time O(N^2) Space O(N)"""
+        """Greedy: Time O(N), Space O(1)"""
+        n = len(nums)
+        
+        # base cases
+        if n == 0 or n == 1:
+            return n
+        if n == 2:
+            return 1 if nums[0] == nums[1] else 2
+        
+        # prev_pos: diff between elements at i-2 and i-1 was positive
+        # prev_neg: diff between elements at i-2 and i-1 was negative
+        # (ignoring consecutive same elements in the array)
+        if nums[0] == nums[1]:
+            prev_pos = True
+            prev_neg = True
+            max_ = 1
+        else:
+            prev_pos = nums[0] < nums[1]
+            prev_neg = nums[0] > nums[1]
+            max_ = 2
+        for i in range(2, n):
+            # is diff between elements at i-1 and i pos or neg?
+            if nums[i-1] < nums[i]:
+                # if positive and previous was negative, increment and switch prev_pos/neg
+                if prev_neg:
+                    max_ += 1
+                    prev_pos = True
+                    prev_neg = False
+            elif nums[i-1] > nums[i]:
+                # if neg and previous was pos, increment and switch prev_pos/neg
+                if prev_pos:
+                    max_ += 1
+                    prev_pos = False
+                    prev_neg = True
+        return max_
+    def wiggleMaxLengthDP(self, nums: List[int]) -> int:
+        """Dynamic Prog: Time O(N^2) Space O(N)"""
         n = len(nums)
         
         # base cases
