d84d5f974df13166c81737cb23433c28c5d17285
raphstrebel <raphael.strebel@epfl.ch>
2022-05-03 20:08:14 +0200

    find path if exists in graph

---
+++ Graph/DFS/1. find-if-path-exists-in-graph.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        if source == destination:
+            return True
+        if len(edges) == 0:
+            return False
+        
+        # build linked list to navigate through edges easily
+        edge_dict = defaultdict(list)
+        for x, y in edges:
+            edge_dict[y].append(x)
+            edge_dict[x].append(y)
+        
+        # Depth-First Search
+        visited = set()
+        stack = []
+        # start at source, no need to iterate over all connected components
+        stack.append(source)
+        while len(stack) > 0:
+            # retrieve first element of list
+            x = stack.pop(0)
+            # if the element is visited, all neighbours are in stack
+            if x in visited:
+                continue
+            # add all neighbours of x that have not been visited
+            for y in edge_dict[x]:
+                if not y in visited:
+                    if y == destination:
+                        return True
+                    stack.append(y)
+            visited.add(x)
+        return False
