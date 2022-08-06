61f7d90ce30da1dab14b51b5032849ac4e6381f7
raphstrebel <raphael.strebel@epfl.ch>
2022-08-06 12:27:28 +0200

    BS: day 6: arrange n coins in stairs (using binary search)

--- Binary Search 1/11. arranging-coins.py
+++ Binary Search 1/11. arranging-coins.py
@@ -1,7 +1,26 @@
 class Solution:
-    def arrangeCoins(self, n: int) -> int:
+    def arrangeCoinsBS(self, n: int) -> int:
         """
-        Using Maths:
+        Using BS:
+        Call number of stairs as m, then 0 <= m <= n
+        (there cannot be more stairs than coins)
+        """
+        
+        start, stop = 0, n
+        while start <= stop:
+            m = (start + stop) // 2
+            m_stairs = m * (m + 1) // 2
+            if m_stairs == n:
+                return m
+            if m_stairs < n:
+                start = m + 1
+            else:
+                stop = m - 1
+        return stop
+
+    def arrangeCoinsMath(self, n: int) -> int:
+        """
+        Using Math:
         n = 1 + 2 + ... + m + eps, eps < m
         <-> n = m * (m + 1) / 2 + eps
         (eps < m, n = math.floor(m * (m + 1) / 2 + eps))
